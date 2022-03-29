9c78b3f8cfb7790615e17d023a63f02090e7e619
Christopher Booth <christopher.booth@plymouth.ac.uk>
2022-03-29 14:26:15 +0100

    initial commit

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDjGA5gsYY8GpcqSE1nF0XnuAr3zDYNL_I",
+    authDomain: "firefive-ba872.firebaseapp.com",
+    projectId: "firefive-ba872",
+    storageBucket: "firefive-ba872.appspot.com",
+    messagingSenderId: "910834649648",
+    appId: "1:910834649648:web:2208b27deb89091fa0fbf5"
+});
+
+var db = firebase.firestore();
